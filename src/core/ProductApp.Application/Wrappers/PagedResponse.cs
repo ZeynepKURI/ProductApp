59539226f8df328f77e1371bebf5e641e23131e4
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2024-12-09 14:39:01 +0300

    güncel

---
+++ src/core/ProductApp.Application/Wrappers/PagedResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ProductApp.Application.Wrappers
+{
+	public class PagedResponse<T>: ServicerResponse<T>
+	{
+		public int PageNumber { get; set; }
+
+		public int PageSize { get; set; }
+
+		public PagedResponse(T value): base(value)
+		{
+
+		}
+
+        public PagedResponse()
+        {
+            PageNumber = 1;
+            PageSize = 10;
+        }
+
+
+        public PagedResponse(int pageNumber, int pageSize)
+		{
+			pageNumber = pageNumber;
+			pageSize = pageSize;
+		}
+	}
+}
+
---
+++ src/core/ProductApp.Application/Wrappers/ServicerResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProductApp.Application.Wrappers
+{
+	public class ServicerResponse
+	{
+		public ServicerResponse()
+		{
+		}
+	}
+}
+
